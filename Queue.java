4feae952655d9887f593b7e3745a589697651625
Hazar-Ozbayhan <76775041+Hazar-Ozbayhan@users.noreply.github.com>
2022-05-17 17:41:49 +0300

    Project Upload

---
+++ Queue.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project1;
+
+public class Queue {
+    Point ﬁrst;
+    Point last;
+
+    public Queue() {
+        ﬁrst = null;
+        last = null;
+    }
+
+    boolean isEmpty() {
+        if (ﬁrst == null) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    void enqueue(Point p) {
+        if (!isEmpty()) {
+            last.setNext(p);
+        } else {
+            ﬁrst = p;
+        }
+        last = p;
+    }
+
+    Point dequeue() {
+        Point result;
+        result = ﬁrst;
+        if (!isEmpty()) {
+            ﬁrst = ﬁrst.getNext();
+        }
+        return result;
+    }
+}
